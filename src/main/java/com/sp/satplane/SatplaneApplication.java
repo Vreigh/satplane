2e1d080b28be9b643dadb0ce801b5196bd7e1b3a
filip <filip.raszka3@gmail.com>
2020-05-24 15:41:56 +0200

    init commit

---
+++ src/main/java/com/sp/satplane/SatplaneApplication.java
@@ -0,0 +1,13 @@
+package com.sp.satplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatplaneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SatplaneApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sp/satplane/SatplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.satplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatplaneApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
